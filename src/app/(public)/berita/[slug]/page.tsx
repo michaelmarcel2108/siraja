f7e36da2f3c2c9da7855cf7834cba913f210b501
michaelmarcel2108 <michaelmhartono@gmail.com>
2026-06-12 21:03:59 +0800

    deploy try 4th

---
+++ src/app/(public)/berita/[slug]/page.tsx
@@ -0,0 +1,10 @@
+// src/app/(public)/berita/[slug]/page.tsx
+
+export default function BacaBerita() {
+  return (
+    <div className="container mx-auto p-8 text-center pt-32">
+      <h1 className="text-2xl font-bold">Halaman Baca Berita</h1>
+      <p>Fitur ini sedang dalam tahap pengembangan...</p>
+    </div>
+  );
+}
